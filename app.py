76386132b1555197d7f0bed1b112500e8dd23493
agarwalkritika218-sys <agarwalkritika218@gmail.com>
2026-01-22 22:33:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,6 @@ def add_event():
 # RUN APP
 # =========================
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
